28a54e42b542473706428727e22bc49c34b32c8f
harvset7777 <333harvest.moon333@gmail.com>
2025-04-14 20:33:05 -0700

    asdf

--- app/inventory/_components/ProductDisplay.tsx
+++ app/inventory/_components/ProductDisplay.tsx
@@ -8,6 +8,12 @@ interface ProductDisplayProps {
 }
 const ProductDisplay: React.FC<ProductDisplayProps> = ({ product }) => {
   const [visible, setVisible] = useState<boolean>(false);
+  const getQuantityColor = (quantity: number) => {
+    if (quantity <= 0) return "text-red-500";
+    if (quantity <= 10) return "text-yellow-500";
+    return "text-green-500";
+  };
+
   return (
     <div className="w-60 overflow-hidden rounded-xl outline-1 outline-muted p-2">
       <ProductInfoModal product={product} visible={visible} setVisible={setVisible} />
@@ -15,7 +21,7 @@ const ProductDisplay: React.FC<ProductDisplayProps> = ({ product }) => {
         image would go here
       </div>
       <h1 className="font-bold">{product.name}</h1>
-      <h2>Quantity: {product.quantity}</h2>
+      <h2 className={`${getQuantityColor(product.quantity)}`}>Quantity: {product.quantity}</h2>
       <span onClick={() => setVisible(true)} className="hover:cursor-pointer flex gap-x-2 items-center ">
         <BsInfoCircle />
         More info
